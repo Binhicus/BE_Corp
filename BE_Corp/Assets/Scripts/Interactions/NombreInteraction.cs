c42c05d006252ea865f0a147407cd7d85c82d99f
Foucart <thomas.foucart@outlook.fr>
2021-11-29 15:27:22 +0100

    singe

---
+++ BE_Corp/Assets/Scripts/Interactions/NombreInteraction.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NombreInteraction : MonoBehaviour
+{
+    public int NbreInteractionTotale;
+    public int NbreInteractionActuelle;
+    public GameObject Papillon3;
+    public GameObject Papillon2;
+    public GameObject Papillon1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    if(NbreInteractionTotale==3){
+        
+        if(NbreInteractionActuelle==3)
+        {
+            Papillon1.SetActive(true);
+            Papillon2.SetActive(true);
+            Papillon3.SetActive(true);
+        }
+
+        if(NbreInteractionActuelle==2)
+        {
+            Papillon1.SetActive(true);
+            Papillon2.SetActive(true);
+            Papillon3.SetActive(false);
+        }
+
+        if(NbreInteractionActuelle==1)
+        {
+            Papillon1.SetActive(true);
+            Papillon2.SetActive(false);
+            Papillon3.SetActive(false);
+        }
+        if(NbreInteractionActuelle==0)
+        {
+            Papillon1.SetActive(false);
+            Papillon2.SetActive(false);
+            Papillon3.SetActive(false);
+        }
+    }
+
+    }
+
+    public void PerdInteraction()
+    {
+        if(NbreInteractionActuelle>0)
+        {
+            NbreInteractionActuelle-=1;
+        }
+    }
+}
---
+++ BE_Corp/Assets/Scripts/Inventaire/Pickup.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pickup : MonoBehaviour
+{
+    public Inventory inventory;
+    public Sprite itemButton;
+    public GameObject ButtonChoix;
+    public NombreInteraction nombreInteraction;
+    public bool ObjetRecupere;
+    public bool DebloqueNouveauxChoix;
+    public List<GameObject> nouveauxChoix = new List<GameObject>();
+    public int NumeroStockageSlot;
+
+    public void Ramasse()
+    {
+        Debug.Log("Appuie");
+        for (int i = 0; i < inventory.slots.Length; i++)
+        {
+                if(inventory.isFull[i] == false)
+                {
+                    // L'item peut être ajouté
+                    inventory.isFull[i] = true;
+                    inventory.slots[i].GetComponent<Image>().sprite=itemButton;
+                    Instantiate(itemButton, inventory.slots[i].transform);
+                    NumeroStockageSlot=i;
+                    ButtonChoix.SetActive(false);
+                    Debug.Log("Ajout");
+                   // nombreInteraction.PerdInteraction();
+                    ObjetRecupere=true;
+
+                    if(DebloqueNouveauxChoix)
+                    {
+                        for (int j = 0; j < nouveauxChoix.Count; j++)
+                        {
+                            nouveauxChoix[j].SetActive(true);
+                        }
+                    }
+                    break;
+                }
+        }
+    }
+    
+}
