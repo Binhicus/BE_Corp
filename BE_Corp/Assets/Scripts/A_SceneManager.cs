79376191ee4fb752abc8474828f1089c4f17f015
Mayzhy <mayzhy.axelle@hotmail.com>
2021-12-05 15:24:38 +0100

    Scene and scene management created

---
+++ BE_Corp/Assets/Scripts/A_SceneManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class A_SceneManager : MonoBehaviour
+{
+    //permet de changer de scène grace à des boutons
+
+    public string mainMenu;
+    public string settingsMenu;
+    public string tutorialLevel;
+    public void mainMenuScene()
+    {
+        SceneManager.LoadScene(mainMenu);
+        
+    }
+
+    public void settingsMenuScene()
+    {
+        SceneManager.LoadScene(settingsMenu);
+    }
+
+    public void tutorialLevelScene()
+    {
+        SceneManager.LoadScene(tutorialLevel);
+    }
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
