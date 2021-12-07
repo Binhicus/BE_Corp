390f7d8eb81ed3e30a8ae7e39fa135c54490d524
Antoine Letailleur <ant.letailleur@gmail.com>
2021-12-07 15:45:28 +0100

    Mouvement V2 + Player visible à travers les obstacles

--- BE_Corp/Assets/Scripts/Player/PlayerController.cs
+++ BE_Corp/Assets/Scripts/Player/PlayerController.cs
@@ -10,6 +10,7 @@ public class PlayerController : MonoBehaviour
     float axis_X, axis_Y;
 
     public float speed = 5f;
+    public float rotationSpeed = 720;
 
     // Start is called before the first frame update
     void Start()
@@ -20,8 +21,17 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.right * speed * axis_X * Time.deltaTime);
-        transform.Translate(Vector3.forward * speed * axis_Y * Time.deltaTime);
+        Vector3 movementDirection = new Vector3(axis_X, 0, axis_Y);
+        movementDirection.Normalize();
+
+        transform.Translate(movementDirection * speed * Time.deltaTime, Space.World);
+
+        if (movementDirection != Vector3.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
+
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+        }
     }
 
     void OnHorizontal(InputValue val)
