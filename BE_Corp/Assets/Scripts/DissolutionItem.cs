c59c7e5b953e6ce8f52a2236f6c9be6f1d7645b0
Foucart <thomas.foucart@outlook.fr>
2021-12-09 16:07:31 +0100

    Switch

--- BE_Corp/Assets/Scripts/DissolutionItem.cs
+++ BE_Corp/Assets/Scripts/DissolutionItem.cs
@@ -4,10 +4,23 @@ using UnityEngine;
 
 public class DissolutionItem : MonoBehaviour
 {
-    public List<Animator> listAnimDiss = new List<Animator>();
-    public List<Animator> listAnimApp = new List<Animator>();
+    public List<GameObject> listObjetDiss = new List<GameObject>();
+    public List<BoxCollider> listObjetDissCollider = new List<BoxCollider>();
+    public List<Outline> listObjetDissOutline = new List<Outline>();
+    public List<GameObject> listObjetDissE = new List<GameObject>();
+    public List<BoxCollider> listPhysicDiss = new List<BoxCollider>();   // Pour que l'objet n'ai plus de physique
+
+    public List<GameObject> listObjetApp = new List<GameObject>();
+    public List<BoxCollider> listObjetAppCollider = new List<BoxCollider>();
+    public List<Outline> listObjetAppOutline = new List<Outline>();
+    public List<GameObject> listObjetAppE = new List<GameObject>();
+    public List<BoxCollider> listPhysicApp = new List<BoxCollider>();       // Pour que l'objet n'ai plus de physique
+
     public AudioSource Disso;
     public GameObject MettreAJourSouvenir;
+    public GameObject BackSouvenir;
+    public bool Dis;
+    public float temps;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,54 +30,105 @@ public class DissolutionItem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-    }
-
-    public void Dissolution()
-    {
-        Disso.Play();
-         for (int i = 0; i < listAnimDiss.Count; i++)
+        if(Input.GetKeyDown(KeyCode.Space)&&!Dis)
         {
-            listAnimDiss[i].SetTrigger("Disparait");
+            DisAp();
+            
+            StartCoroutine(coroutineA());
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space)&&Dis)
+        {
+            ApDis();
+            
+            StartCoroutine(coroutineB());
         }
     }
-    public void Apparait()
+
+    /*public void Dissolution()
+    {
+        Disso.Play();
+         for (int i = 0; i < listObjetDiss.Count; i++)
+        {
+            listObjetDiss[i].GetComponent<Animation>().SetTrigger("Disparait");
+
+        }
+
+            StartCoroutine(coroutineAttend());
+
+        for (int i = 0; i < listObjetDiss.Count; i++)
+        {
+            listObjetDiss[i].SetActive(false);
+
+        }
+
+    }
+    /*public void Apparait()
     {
         Disso.Play();
         for (int i = 0; i < listAnimApp.Count; i++)
         {
             listAnimApp[i].SetTrigger("Apparait");
         }
-    }
+    }*/
 
     public void DisAp()
     {
         Disso.Play();
-         for (int i = 0; i < listAnimDiss.Count; i++)
+         for (int i = 0; i < listObjetDiss.Count; i++)
         {
-            listAnimDiss[i].SetTrigger("Disparait");
-            Debug.Log("disparait allez");
+            listObjetDiss[i].GetComponent<Animator>().SetTrigger("Disparait");
+            listObjetDissCollider[i].enabled=false;
+            listObjetDissOutline[i].enabled=false;
+            listObjetDissE[i].SetActive(false);
+            listPhysicDiss[i].enabled=false;
         }
 
-        for (int j = 0; j < listAnimApp.Count; j++)
+        for (int j = 0; j < listObjetApp.Count; j++)
         {
-            listAnimApp[j].SetTrigger("Apparait");
+            listObjetApp[j].GetComponent<Animator>().SetTrigger("Apparait");
+            listObjetAppCollider[j].enabled=true;
+            listPhysicApp[j].enabled=true;
         }
-
-        
-
-        //StartCoroutine(coroutineA());
     }
 
-    /* IEnumerator coroutineA()
+    public void ApDis()
+    {
+        Disso.Play();
+        for (int j = 0; j < listObjetApp.Count; j++)
+        {
+            listObjetApp[j].GetComponent<Animator>().SetTrigger("Disparait");
+            listObjetAppCollider[j].enabled=false;
+            listObjetAppOutline[j].enabled=false;
+            listObjetAppE[j].SetActive(false);
+            listPhysicApp[j].enabled=false;
+        }
+
+        for (int i = 0; i < listObjetDiss.Count; i++)
+        {
+            listObjetDiss[i].GetComponent<Animator>().SetTrigger("Apparait");
+            listObjetDissCollider[i].enabled=true;
+            listPhysicDiss[i].enabled=true;
+        }
+        /*MettreAJourSouvenir.SetActive(true);
+        MettreAJourSouvenir.GetComponent<Animator>().SetTrigger("Choix");
+        BackSouvenir.GetComponent<Animator>().SetTrigger("Retour");
+        BackSouvenir.SetActive(false);*/
+
+
+    }
+
+     IEnumerator coroutineA()
     {
         
-        yield return new WaitForSeconds(1.0f);
-        Disso.Play();
-        for (int i = 0; i < listAnimApp.Count; i++)
-        {
-            listAnimApp[i].SetTrigger("Apparait");
-        }
-        MettreAJourSouvenir.SetActive(false);
-    }*/
+        yield return new WaitForSeconds(0.3f);
+        Dis=true;
+    }
+
+    IEnumerator coroutineB()
+    {
+        
+        yield return new WaitForSeconds(0.3f);
+        Dis=false;
+    }
 }
