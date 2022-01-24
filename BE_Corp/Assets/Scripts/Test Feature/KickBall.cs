3484cd9dcaede008666b474d2c39497d7549935b
Binhicus <abdoulaye-ba@live.fr>
2022-01-24 19:24:32 +0100

    début interaction salon part 1
    Ballon + Vase

---
+++ BE_Corp/Assets/Scripts/Test Feature/KickBall.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KickBall : MonoBehaviour
+{
+    public float hauteur = 1.0f;
+    public float allant = 3.0f;
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Kicked()
+    {
+        rb.AddForce(0, hauteur, allant, ForceMode.Impulse);
+    } 
+}
