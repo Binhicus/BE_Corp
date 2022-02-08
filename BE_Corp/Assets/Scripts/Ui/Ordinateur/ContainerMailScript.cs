3f7b41c5ec1779d1f1954cb6bb3962de644d34ce
NioZebra <antonin.mercier@e-artsup.net>
2022-02-08 19:11:00 +0100

    Calcule de la Date

--- BE_Corp/Assets/Scripts/Ui/Ordinateur/ContainerMailScript.cs
+++ BE_Corp/Assets/Scripts/Ui/Ordinateur/ContainerMailScript.cs
@@ -10,12 +10,12 @@ public class ContainerMailScript : MonoBehaviour
     public List<Mail> AllMail ;
 
     private List<Mail> MailSort = new List<Mail>() ;
-
+    [Space(25)]
     public GameObject PrefabDateRibbon ;
     public GameObject PrefabMailDisplayer ;
 
     public bool AllMailAsRead ;
-    /*[HideInInspector] */public string CurrentDate ;
+    [HideInInspector] public string CurrentDate ;
     private string[] DateJour = new string[] {"Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Diamnche"} ;
     private string[] DateMois = new string[] {"Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"} ;
 
@@ -118,7 +118,6 @@ public class ContainerMailScript : MonoBehaviour
     {
         string GetRibbonText = "" ;
 
-
         if(GetDate(DateBeChecked) > GetDate(DateRef))    GetRibbonText = "Mail du FUTUR" ;
 
         if(GetDate(DateBeChecked) == GetDate(DateRef))    GetRibbonText = "Aujourd'hui" ;
@@ -135,29 +134,80 @@ public class ContainerMailScript : MonoBehaviour
 
     string GetDayOfTheDate(string Date)
     {
-        string DaysReturn = "" ;
-        string[] Days = new string[] {"lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi", "dimanche"} ;
+        //string DaysReturn = "" ;
+        string[] Days = new string[] {"dimanche", "lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi"} ;
         string[] Dates = Date.Split('/') ; //  JJ / MM / YYYY      
 
-        int Etape1, Etape2, Etape3, Etape4, Etape5, Etape6, Etape7 = 0 ;
-
+        int Etape1 = 0 ; 
+        int Etape2 = 0 ;
+        int Etape3 = 0 ; 
+        int Etape4 = 0 ;
+        int Etape5 = 0 ;
+        int Etape6 = 0 ;
 
         /* Etape 1 : Avoir le jour du mois */
         Etape1 = int.Parse(Dates[0]) ;
 
+
         /* Etape 2 : Enlever les siècle et ne gardez que les décénies */
+        int Decade = int.Parse(Dates[2].Substring(Dates[2].Length - 2));
+        Etape2 = Decade ;
 
 
-        /* Etape 3 : Divisez par 4 pour retire les années bisextile, si le résultat est net et que le mois est janvier ou février, on retire 1, si le résultat est décimale, on ajoute 1 */
+        /* Etape 3 : Récupérer le nombre d'année bisextile, Divisez par 4 pour retire les années bisextile, si le résultat est net et que le mois est janvier ou février, on retire 1, si le résultat est décimale, on ajoute 1 */
+        int NmBisYears = 0;
+        while(Decade > 3)
+        {
+            Decade -= 4 ;            
+            NmBisYears ++ ;
+        }
+
+        if(Decade != 0) Etape3 = NmBisYears + 1 ;
+        else {
+            if((int.Parse(Dates[1]) < 3)) Etape3 = NmBisYears - 1 ; 
+            if((int.Parse(Dates[1]) >= 3)) Etape3 = NmBisYears  ; 
+        }
+
+
         /* Etape 4 : Ajouté un indicatif par mois ;     J:+0  |  F:+3  |  M:+3  |  A:+6  |  M:+1  |  J:+4  |  J:+6  |  A:+2  |  S:+5  |  O:+0  |  N:+3  |  D+5      */
+        if(Dates[1] == "01" || Dates[1] == "10") Etape4 = 0 ;
+        if(Dates[1] == "02" || Dates[1] == "03" || Dates[1] == "11") Etape4 = 3 ;
+        if(Dates[1] == "04" || Dates[1] == "07") Etape4 = 6 ;
+        if(Dates[1] == "05") Etape4 = 1 ;
+        if(Dates[1] == "06") Etape4 = 4 ;
+        if(Dates[1] == "08") Etape4 = 2 ;
+        if(Dates[1] == "09" || Dates[1] == "12") Etape4 = 5 ;
+
+
         /* Etape 5 : Ajouté un indicatif par sicèle ;   Si,     Siècles/4=x,00: +6   |   Siècle/4=x,25: +4   |   Siècle/4=x,5: +2  |  Siècle/4=x,75: +0     */
+        float Siecle = int.Parse(Dates[2].Remove(Dates[2].Length - 2)); 
+        while(Siecle > 3)
+        {
+            Siecle -= 4 ;
+        }
+
+        if(Siecle == 0) Etape5 = 6 ;
+        if(Siecle == 1) Etape5 = 4 ;
+        if(Siecle == 2) Etape5 = 2 ;
+        if(Siecle == 3) Etape5 = 0 ;
+
+
         /* Etape 6 : On additionne toute les étapes précédentes */
+        Etape6 = Etape1 + Etape2 + Etape3 + Etape4 + Etape5 ; 
+        Debug.Log(Etape6 + " = " + Etape1 + " + " + Etape2 + " + " + Etape3 + " + " + Etape4 + " + " + Etape5);
+        
+
         /* Etape 7 : On fait le modulo 7 du résulta de l'étape 6, le reste donne le jour :      0:Dimanche  |  1:Lundi  |  2:Mardi  |  3:Mercredi  |  4:Jeudi  |  5:Vendredi  |  6:Samedi */
+        int Modulo = Etape6 ;
+        while(Modulo >= 7)
+        {
+            Modulo -= 7 ;
+        }
+        Debug.Log(Modulo);
 
-
-
-        return DaysReturn ;
+        return Days[Modulo] ;
     }
+
 
     string GetPartOfTheDate(string Date, int PartNeed)
     {
