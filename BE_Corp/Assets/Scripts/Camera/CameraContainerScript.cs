7bd6cf550929ada382d7ed1ead939effbd548700
NioZebra <antonin.mercier@e-artsup.net>
2022-04-06 15:21:57 +0200

    Camera Container

---
+++ BE_Corp/Assets/Scripts/Camera/CameraContainerScript.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraContainerScript : MonoBehaviour
+{
+    [Header ("Cuisine Camera")]
+    public GameObject CameraRadio ;
+}
