cbd1caf8aa14bc692d430575647a98dd107a7eb9
Binhicus <abdoulaye-ba@live.fr>
2022-04-06 12:24:48 +0200

    ajout interaction sur objets salon

--- BE_Corp/Assets/Scripts/Objects/BallInteraction.cs
+++ BE_Corp/Assets/Scripts/Objects/BallInteraction.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BallInteraction : MonoBehaviour, IClicked
+public class BallInteraction : ClickableObject, IClicked, IAction
 {
     private KickBall kickBall;
     public string nomDuBallon;
+
+    public List<ActionWheelChoiceData> ListInteractPossible = new List<ActionWheelChoiceData>();
 
 
     private void OnEnable()
@@ -14,8 +16,28 @@ public class BallInteraction : MonoBehaviour, IClicked
     }
     public void OnClickAction()
     {
-        kickBall.Kicked();
+        CursorController.Instance.ActionWheelScript.ChoicesDisplay = ListInteractPossible;
+        CursorController.Instance.ActionWheelScript.TargetAction = this;
+        CursorController.Instance.ActionWheelScript.gameObject.SetActive(true);
     }
 
+    public void OnOpen() { Debug.Log("Open"); }
+    public void OnClose() { Debug.Log("Close"); }
 
+    public void OnTake()
+    {
+        Debug.Log("Taken");
+    }
+
+    public void OnUse() 
+    {
+        kickBall.Kicked();
+    }
+    public void OnInspect() { Debug.Log("Inspect"); }
+    public void OnQuestion() { Debug.Log("Question"); }
+
+    public void OnLunchActionAfterCloseDialogue()
+    {
+
+    }
 }
