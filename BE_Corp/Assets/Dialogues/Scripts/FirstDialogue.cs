b145d3852009e799fb07d771454b9458806babc4
Binhicus <abdoulaye-ba@live.fr>
2022-04-08 10:46:49 +0200

    1e ref fungus

---
+++ BE_Corp/Assets/Dialogues/Scripts/FirstDialogue.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class FirstDialogue : MonoBehaviour
+{
+    public BlockReference blockRef;
+    void Start()
+    {
+        Invoke("DialogDebut", 3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DialogDebut()
+    {
+        blockRef.Execute();
+    }
+}
