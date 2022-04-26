ecfc5be1472af4c4e44d342d6ecf472b121c777d
Binhicus <abdoulaye-ba@live.fr>
2022-04-26 12:08:12 +0200

    effet glitch ajouté vamooooossss

--- BE_Corp/Assets/Dialogues/Scripts/FirstDialogue.cs
+++ BE_Corp/Assets/Dialogues/Scripts/FirstDialogue.cs
@@ -8,7 +8,7 @@ public class FirstDialogue : MonoBehaviour
     public BlockReference blockRef;
     void Start()
     {
-        Invoke("DialogDebut", 3);
+        Invoke("DialogDebut", 1);
     }
 
     // Update is called once per frame
