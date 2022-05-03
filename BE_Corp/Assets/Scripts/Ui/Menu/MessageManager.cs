46a14cd419680203795b18613a625f87cc3d04ea
NioZebra <antonin.mercier@e-artsup.net>
2022-05-03 23:56:19 +0200

    Dialog Introduction Niveau

---
+++ BE_Corp/Assets/Scripts/Ui/Menu/MessageManager.cs
@@ -0,0 +1,251 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+using TMPro;
+using DG.Tweening;
+using UnityEngine.UI;
+
+enum DifferentNameInMessage{
+Bots,
+Employee,
+Client
+}
+public class MessageManager : MonoBehaviour
+{
+    public string CurrentName ;
+    public Text StoryTextReference ;
+    public Text CompleteStoryTextRefence ;
+    public Flowchart FlowchartTextMenu ;
+    public Text ClientMessageText ;
+
+    public Transform DiscussionDisplay ;
+    public TextMeshProUGUI DiscussionWriterText ;
+    public GameObject BotsNarratorTextPrefab;
+    public GameObject EmployeeTextPrefab;    
+    public GameObject ClientTextPrefab;
+    public GameObject ClientWritePrefab ;
+    private GameObject ClientWritePrefabInst ;
+
+
+    public BlockReference BR ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CurrentName = FlowchartTextMenu.GetStringVariable("Name"); 
+
+        if(FlowchartTextMenu.GetBooleanVariable("ClientWrite"))
+        {
+            if(StoryTextReference.text.Length != ClientMessageText.text.Length)
+            {
+                if(!TheClientWritePrefabInst) InstantiateClientWrite() ; // InstantiateWriteAnimation
+            } else {
+                FlowchartTextMenu.SetBooleanVariable("ClientWrite", false);                
+                TheClientWritePrefabInst = false ;
+                Destroy(ClientWritePrefabInst);
+                DisplayCompleteClientMessage();               
+                StopAllCoroutines(); 
+            }
+        }
+
+        if(FlowchartTextMenu.GetBooleanVariable("EmployeeWrite"))
+        {          
+            if(StoryTextReference.text.Length != CompleteStoryTextRefence.text.Length)
+            {
+                DiscussionWriterText.text = StoryTextReference.text ;
+                if(!WriteCoroutineLunch) StartCoroutine(HeightDuringWriting(725f, DiscussionWriterText));
+            } else {
+                FlowchartTextMenu.SetBooleanVariable("EmployeeWrite", false);     
+                SetFinalHeight(725f, DiscussionWriterText);
+                DiscussionWriterText.text = StoryTextReference.text ;                           
+                StopAllCoroutines(); 
+            }
+        }
+    }
+
+    public void CallNextDialog()
+    {
+        FlowchartTextMenu.SetBooleanVariable("CanDisplayNext", false) ; 
+        NewDialog();
+    }
+
+    public void SendEmployeeMessage()
+    {   if(CurrentName == DifferentNameInMessage.Employee.ToString())
+        {
+            DiscussionWriterText.text = "..." ;
+            SetFinalHeight(725f, DiscussionWriterText);
+
+            GameObject EmployeeTextIns = Instantiate(EmployeeTextPrefab, DiscussionDisplay.transform);
+            EmployeeTextIns.transform.SetSiblingIndex(1);
+            EmployeeTextIns.GetComponentInChildren<TextMeshProUGUI>().text = StoryTextReference.text ;
+
+            Vector2 NewSize ;
+
+            if(EmployeeTextIns.GetComponentInChildren<TextMeshProUGUI>().preferredWidth < 460f)
+            {
+                NewSize.x = EmployeeTextIns.GetComponentInChildren<TextMeshProUGUI>().preferredWidth ;
+            } else {
+                NewSize.x =  460f ;
+            }
+
+            int NumDivision = 0 ;
+
+            while((EmployeeTextIns.GetComponentInChildren<TextMeshProUGUI>().preferredWidth - (445f * NumDivision)) > 445f)
+            {
+                NumDivision++;
+            }
+
+            NewSize.y =  50f + 28f * NumDivision ; 
+
+            EmployeeTextIns.transform.GetChild(0).GetComponent<RectTransform>().sizeDelta = new Vector2(NewSize.x + 15f, NewSize.y) ;
+            EmployeeTextIns.GetComponent<RectTransform>().sizeDelta = new Vector2(EmployeeTextIns.GetComponent<RectTransform>().sizeDelta.x, NewSize.y + 15f)  ;
+
+            StartCoroutine(WaitBeforeAutorizedNextDialogue(0.5f));  
+        }  
+    }
+
+    bool TheClientWritePrefabInst = false ;
+    void InstantiateClientWrite()
+    {
+        TheClientWritePrefabInst = true ;
+        GameObject ClientWriteIns = Instantiate(ClientWritePrefab, DiscussionDisplay.transform);
+        ClientWritePrefabInst = ClientWriteIns ;   
+        ClientWritePrefabInst.transform.SetSiblingIndex(1);
+    }
+
+    void DisplayCompleteClientMessage()
+    {
+        GameObject ClientTextIns = Instantiate(ClientTextPrefab, DiscussionDisplay.transform);
+        ClientTextIns.transform.SetSiblingIndex(1);
+        ClientTextIns.GetComponentInChildren<TextMeshProUGUI>().text = StoryTextReference.text ;
+
+        Vector2 NewSize ;
+
+        if(ClientTextIns.GetComponentInChildren<TextMeshProUGUI>().preferredWidth < 455f)
+        {
+            NewSize.x = ClientTextIns.GetComponentInChildren<TextMeshProUGUI>().preferredWidth ;
+        } else {
+            NewSize.x =  460f ;
+        }
+
+        int NumDivision = 0 ;
+
+        while((ClientTextIns.GetComponentInChildren<TextMeshProUGUI>().preferredWidth - (445f * NumDivision)) > 445f)
+        {
+            NumDivision++;
+        }
+
+        NewSize.y =  50f + 28f * NumDivision ; 
+
+        ClientTextIns.transform.GetChild(0).GetComponent<RectTransform>().sizeDelta = new Vector2(NewSize.x + 15f, NewSize.y) ;
+        ClientTextIns.GetComponent<RectTransform>().sizeDelta = new Vector2(ClientTextIns.GetComponent<RectTransform>().sizeDelta.x, NewSize.y + 15f)  ;          
+    }
+
+    public void DisplayMission()
+    {
+        if(DiscussionDisplay.childCount < 2)    BR.Execute();
+    }
+
+    void NewDialog()
+    {
+        if(CurrentName == DifferentNameInMessage.Bots.ToString())
+        {
+            GameObject BotsNarratorIns = Instantiate(BotsNarratorTextPrefab, DiscussionDisplay.transform);
+            BotsNarratorIns.GetComponentInChildren<TextMeshProUGUI>().text = StoryTextReference.text ;
+            BotsNarratorIns.GetComponent<RectTransform>().sizeDelta = new Vector2(BotsNarratorIns.GetComponent<RectTransform>().sizeDelta.x, BotsNarratorIns.GetComponentInChildren<TextMeshProUGUI>().preferredHeight + 10f)  ;
+            BR.block.DOTogglePause();            
+            StartCoroutine(WaitBeforeAutorizedNextDialogue(0.1f));
+        }  
+
+    /*    if(CurrentName == DifferentNameInMessage.Employee.ToString())
+        {
+            FlowchartTextMenu.SetBooleanVariable("EmployeeWrite", true);
+        }  
+
+        if(CurrentName == DifferentNameInMessage.Client.ToString())
+        {
+            FlowchartTextMenu.SetBooleanVariable("ClientWrite", true);
+        }        */
+    }
+
+    void GetsAllPrecedentDialog()
+    {
+
+    }
+
+    void SetHeightWriterBox(float WidthMax, TextMeshProUGUI TMPCurrent)
+    {
+        Vector2 NewSize ;
+        int NumDivision = 0 ;
+
+        while((TMPCurrent.preferredWidth - (TMPCurrent.preferredWidth * NumDivision)) > WidthMax)
+        {
+            NumDivision++;
+        }
+
+        NewSize.y =  50f + (15f * NumDivision) ; 
+
+        TMPCurrent.transform.parent.GetComponent<RectTransform>().sizeDelta = new Vector2(TMPCurrent.transform.parent.GetComponent<RectTransform>().sizeDelta.x, NewSize.y)  ;
+        TMPCurrent.transform.parent.transform.parent.GetComponent<RectTransform>().sizeDelta = new Vector2(TMPCurrent.transform.parent.transform.parent.GetComponent<RectTransform>().sizeDelta.x, NewSize.y)  ;
+    }
+
+
+    bool WriteCoroutineLunch = false ;
+    IEnumerator HeightDuringWriting(float WidthMax, TextMeshProUGUI TMPCurrent)
+    {
+        WriteCoroutineLunch = true;
+        Vector2 NewSize ;
+        int NumDivision = 0 ;
+
+        while( ( (TMPCurrent.preferredWidth - (WidthMax * NumDivision) ) > WidthMax))
+        {
+            NumDivision += 1 ;
+        }
+
+        int GoodHeightIns = 0 ;
+        while(GoodHeightIns <= NumDivision)
+        {
+            NewSize.y =  50f + (28f * GoodHeightIns) ; 
+
+            TMPCurrent.transform.parent.GetComponent<RectTransform>().sizeDelta = new Vector2(TMPCurrent.transform.parent.GetComponent<RectTransform>().sizeDelta.x, NewSize.y)  ;
+            TMPCurrent.transform.parent.transform.parent.GetComponent<RectTransform>().sizeDelta = new Vector2(TMPCurrent.transform.parent.transform.parent.GetComponent<RectTransform>().sizeDelta.x, NewSize.y)  ;  
+            DiscussionDisplay.GetComponent<VerticalLayoutGroup>().spacing = 9 ;
+            DiscussionDisplay.GetComponent<VerticalLayoutGroup>().spacing = 10 ;
+
+            yield return new WaitForSeconds(1.25f) ;    
+            GoodHeightIns++ ;                                          
+        }
+    }
+
+    void SetFinalHeight(float WidthMax, TextMeshProUGUI TMPCurrent)
+    {
+        Vector2 NewSize ;
+        int NumDivision = 1 ;
+
+        while( ( (TMPCurrent.preferredWidth - (WidthMax * NumDivision) ) > WidthMax))
+        {
+            NumDivision += 1 ;
+        }
+
+        NewSize.y =  50f + (25f * NumDivision) ; 
+
+        TMPCurrent.transform.parent.GetComponent<RectTransform>().sizeDelta = new Vector2(TMPCurrent.transform.parent.GetComponent<RectTransform>().sizeDelta.x, NewSize.y)  ;
+        TMPCurrent.transform.parent.transform.parent.GetComponent<RectTransform>().sizeDelta = new Vector2(TMPCurrent.transform.parent.transform.parent.GetComponent<RectTransform>().sizeDelta.x, NewSize.y)  ;  
+        DiscussionDisplay.GetComponent<VerticalLayoutGroup>().spacing = 9 ;
+        DiscussionDisplay.GetComponent<VerticalLayoutGroup>().spacing = 10 ;
+    }
+
+
+    IEnumerator WaitBeforeAutorizedNextDialogue(float Delay)
+    {
+        yield return new WaitForSeconds(Delay);
+        FlowchartTextMenu.SetBooleanVariable("CanDisplayNext", true) ; 
+    }
+}
---
+++ BE_Corp/Assets/Scripts/Ui/Ordinateur/ClientWriteAnimation.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class ClientWriteAnimation : MonoBehaviour
+{
+    public RectTransform LoadImg1 ;
+    public RectTransform LoadImg2 ;
+    public RectTransform LoadImg3 ;
+
+
+    void Start()
+    {
+        StartCoroutine(StartDifferentLoad());
+    }
+
+    IEnumerator StartDifferentLoad()
+    {
+        yield return new WaitForSeconds(0.1f);
+        StartCoroutine(LoadImageAnimation(LoadImg1));
+        yield return new WaitForSeconds(0.1f);
+        StartCoroutine(LoadImageAnimation(LoadImg2));
+        yield return new WaitForSeconds(0.1f);
+        StartCoroutine(LoadImageAnimation(LoadImg3));
+
+    }
+
+    IEnumerator LoadImageAnimation(RectTransform LoadImgRect)
+    {
+        LoadImgRect.DOJumpAnchorPos(new Vector2(0, LoadImgRect.anchoredPosition.y), 10f, 1, 0.25f);
+        yield return new WaitForSeconds(0.4f);
+        StartCoroutine(LoadImageAnimation(LoadImgRect));
+    }
+}
