5511dca7c8a70fecf4753a5935b75f8d3faef5ad
Binhicus <abdoulaye-ba@live.fr>
2022-05-16 14:51:27 +0200

    début jauge

--- BE_Corp/Assets/Scripts/Ui/Jauge.cs
+++ BE_Corp/Assets/Scripts/Ui/Jauge.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class Jauge : MonoBehaviour
 {
+    public GameObject[] orbes;
+    public int stadeProg = 0;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void Illuminate()
     {
-        
+        for (int i = 0; i < orbes.Length; i++)
+        {
+            if(i <= stadeProg - 1)
+            {
+                orbes[i].SetActive(true);
+            }
+        }
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Illuminate();
     }
 }
