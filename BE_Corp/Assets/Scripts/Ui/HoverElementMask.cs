bb030634f7422102ee38c457ff7aefd51c3919ea
NioZebra <antonin.mercier@e-artsup.net>
2022-06-28 15:10:21 +0200

    Dialogue Intro Finito

--- BE_Corp/Assets/Scripts/Ui/HoverElementMask.cs
+++ BE_Corp/Assets/Scripts/Ui/HoverElementMask.cs
@@ -15,11 +15,13 @@ public class HoverElementMask : MonoBehaviour
 
     public void ShowElement()
     {
+        ThisRect.DOKill();
         ThisRect.DOSizeDelta(new Vector2(HeightMax, ThisRect.sizeDelta.y), 0.15f);
     }
 
     public void HideElement()
     {
+        ThisRect.DOKill();
         ThisRect.DOSizeDelta(new Vector2(0f, ThisRect.sizeDelta.y), 0.1f);
     }
 }
