1cb54dc9e1d1cd535d70919236716bde591d31a7
NioZebra <antonin.mercier@e-artsup.net>
2022-07-06 20:55:12 +0200

    Désactivation Dialog Input (Entrée) lors des pause

---
+++ BE_Corp/Assets/Scripts/Ui/DialogueInputPausedController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class DialogueInputPausedController : MonoBehaviour
+{
+    [SerializeField] private DialogInput DI ;
+
+    void FixUpdate()
+    {
+        if(GameObject.Find("Mouse On") == null && DI.enabled)
+        {
+            DI.enabled = false ;
+        }
+
+        if(GameObject.Find("Mouse On") != null && !DI.enabled)
+        {
+            DI.enabled = true ;
+        }
+    }
+}
